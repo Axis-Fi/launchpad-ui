902baff4a486fae8e689d3c2d320902ea48082ff
aphex <wenlogic@protonmail.com>
2024-02-02 13:26:23 +0000

    add create auction page

---
+++ apps/dapp/src/modules/auction/create-auction-submitter.tsx
@@ -0,0 +1,43 @@
+import { Button, Tooltip } from "@repo/ui";
+import { useAllowance } from "loaders/use-allowance";
+import { useAccount } from "wagmi";
+import { CreateAuctionForm } from "./create-auction-page";
+import { useFormContext } from "react-hook-form";
+import { Address } from "viem";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+
+export function CreateAuctionSubmitter() {
+  const { address } = useAccount();
+  const form = useFormContext<CreateAuctionForm>();
+  const { payoutToken, amount } = form.getValues();
+
+  const { isSufficientAllowance, execute, approveTx } = useAllowance({
+    ownerAddress: address,
+    spenderAddress: "0x",
+    tokenAddress: payoutToken?.address as Address,
+    decimals: payoutToken?.decimals,
+    chainId: payoutToken?.chainId,
+    amount: Number(amount),
+  });
+
+  return (
+    <div className="mt-4 flex flex-col items-center justify-center">
+      {!isSufficientAllowance ? (
+        <Button type="submit">Create Auction</Button>
+      ) : (
+        <div className="flex">
+          <Button disabled={approveTx.isLoading} onClick={() => execute()}>
+            {approveTx.isLoading ? "Waiting" : "Approve"}
+          </Button>
+          <Tooltip
+            content={`You need to allow the AuctionHouse contract to spend the configured amount of ${
+              payoutToken?.symbol ?? "the payout token"
+            }`}
+          >
+            <InfoCircledIcon className="ml-1 h-6 w-6" />
+          </Tooltip>
+        </div>
+      )}
+    </div>
+  );
+}
