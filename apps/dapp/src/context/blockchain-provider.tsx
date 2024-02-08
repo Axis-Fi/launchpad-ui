a47209ff34344875206dc212f94130be140312c5
Jem <0x0xjem@gmail.com>
2024-02-08 17:35:30 +0400

    Add react-query devtools

--- apps/dapp/src/context/blockchain-provider.tsx
+++ apps/dapp/src/context/blockchain-provider.tsx
@@ -2,6 +2,7 @@ import { WagmiProvider, createConfig } from "wagmi";
 import { activeConfig } from "../config/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import WalletProvider, { connectors } from "./wallet-provider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ export function BlockchainProvider({
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <WalletProvider>{children}</WalletProvider>
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </WagmiProvider>
   );
