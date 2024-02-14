375f73fe480444fe9a2327e0ea8f29e08fdfb0a3
Jem <0x0xjem@gmail.com>
2024-02-14 14:44:30 +0400

    Implement fetching of bid status from the LSBBA contract

--- apps/dapp/src/modules/auction/auction-bids.tsx
+++ apps/dapp/src/modules/auction/auction-bids.tsx
@@ -1,5 +1,6 @@
 import { BidList } from "./bid-list";
 import { PropsWithAuction } from ".";
+import { useBids } from "loaders/useBid";
 
 type AuctionBidsCard = {
   isLoading?: boolean;
@@ -18,6 +19,13 @@ export function AuctionBidsCard({
   // - "encrypted" base token amount out for each bid (skeleton?)
   // - reveal the decrypted base token amount if there is a matching decrypted bid
   // - display if the bid is refunded (search for it in the refundedBids prop using the bidId)
+
+  const bidResults = useBids(
+    auction?.id,
+    auction.bids.map((b) => b.bidId),
+  );
+  console.log("isLoading = ", bidResults.isLoading);
+  console.log("bids = ", bidResults.data);
 
   return (
     <div {...props}>
