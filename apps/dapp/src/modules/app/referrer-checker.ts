9f23e5ce5d4e0aa597669643ced5834ef7298a79
Oighty <oighty6@gmail.com>
2024-05-09 14:40:19 -0500

    fix: decode referrer and cache in localStorage

--- apps/dapp/src/modules/app/referrer-checker.ts
+++ apps/dapp/src/modules/app/referrer-checker.ts
@@ -1,29 +1,27 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
 import { useSetReferrer } from "state/referral";
-import { isAddress } from "viem";
+import { Address, isAddress } from "viem";
 
 /** Checks if exists and returns a URL param named referrer*/
 export function ReferrerChecker() {
   // Get search params from the URL
   const [searchParams] = useSearchParams();
-  const setReferral = useSetReferrer();
+  const setReferrer = useSetReferrer();
 
   // Get the referrer from the search params, if it exists
-  const encodedReferrer = searchParams.get("ref") ?? "";
+  const encodedReferrer: string = searchParams.get("ref") ?? "";
 
   // Decode the referrer address
-  const referrer = atob(encodedReferrer);
-
-  console.log(referrer);
+  const referrer: string = "0x" + atob(encodedReferrer);
 
   // Parse the referrer into an address, otherwise the referrer is the zero address
   // TODO support address lookup from ENS name in referral?
-  const referrerAddress = isAddress(referrer) ? referrer : null;
+  const referrerAddress: Address | null = isAddress(referrer) ? referrer : null;
 
   React.useEffect(() => {
-    if (referrerAddress) setReferral(referrerAddress);
-  }, [referrerAddress, setReferral]);
+    if (referrerAddress) setReferrer(referrerAddress);
+  }, [referrerAddress, setReferrer]);
 
   return null;
 }
