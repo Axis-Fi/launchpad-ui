6f91a29c247f22020c8907a0dc69b09698b97712
aphex <wenlogic@protonmail.com>
2024-05-15 13:17:17 +0100

    update contract types

--- packages/deployments/src/contracts.ts
+++ packages/deployments/src/contracts.ts
@@ -1,12 +1,13 @@
-import { AxisContractAddresses } from "@repo/types";
+import { AxisCallbackAddresses, AxisContractAddresses } from "@repo/types";
 import { allDeployments } from "./main";
 import { abis } from "@repo/abis";
 
-const addressesPerChain: Record<number, AxisContractAddresses> = allDeployments
-  .flat()
-  .reduce((acc, deployment) => {
-    return { ...acc, [deployment.chain.id]: deployment.addresses };
-  }, {});
+const addressesPerChain: Record<
+  number,
+  AxisContractAddresses & AxisCallbackAddresses
+> = allDeployments.flat().reduce((acc, deployment) => {
+  return { ...acc, [deployment.chain.id]: deployment.addresses };
+}, {});
 
 export const axisContracts = {
   addresses: addressesPerChain,
--- packages/types/src/axis-contracts.ts
+++ packages/types/src/axis-contracts.ts
@@ -19,10 +19,23 @@ export type AxisContractNames =
   | AxisModuleContractNames
   | AxisDerivativeContractNames;
 
+export type AxisCallbackNames =
+  | "dtlUniV2"
+  | "dtlUniV3"
+  | "tokenAllowlist"
+  | "merkleAllowlist"
+  | "cappedMerkleAllowlist";
+
 export type AxisContractAddresses = Record<AxisContractNames, Address>;
+export type AxisCallbackAddresses = Record<AxisCallbackNames, Address>;
+
 export type AxisContractAbis = Record<AxisContractNames, unknown>;
+
+type Contract = { address: Address; abi: unknown[] };
 
 export type AxisContracts = Record<
   number,
-  Record<AxisContractNames, { address: Address; abi: unknown[] }>
+  Record<AxisContractNames, Contract> &
+    //TODO: review, callbacks were made partial for development
+    Partial<Record<AxisCallbackNames, Contract>>
 >;
