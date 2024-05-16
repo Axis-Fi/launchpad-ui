2efeecf89b1ddf694b859230f1896b419462c238
aphex <wenlogic@protonmail.com>
2024-05-16 22:45:41 +0100

    update badge

--- apps/dapp/src/stories/ui/Badge.stories.tsx
+++ apps/dapp/src/stories/ui/Badge.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Badge } from "@repo/ui";
+import icon from "../assets/ohm.png";
 
 const meta = {
   title: "Design System/Badge",
@@ -12,6 +13,7 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     children: "CURATOR",
+    icon,
   },
 };
 
@@ -19,6 +21,17 @@ export const Round: Story = {
   args: {
     children: 70,
     size: "round",
-    variant: "destructive",
+  },
+};
+
+export const CustomContent: Story = {
+  args: {
+    size: "lg",
+    children: (
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-sm">Remaining</p>
+        <p>00:04:20</p>
+      </div>
+    ),
   },
 };
