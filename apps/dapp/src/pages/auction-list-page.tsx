0113efc87246573df66bc878082bc8b86e431bb3
aphex <wenlogic@protonmail.com>
2024-05-23 13:31:09 +0100

    update filters on auction list page

--- apps/dapp/src/pages/auction-list-page.tsx
+++ apps/dapp/src/pages/auction-list-page.tsx
@@ -1,7 +1,6 @@
 import {
   Text,
   Button,
-  DropdownChecker,
   IconnedInput,
   Pagination,
   ToggleGroup,
@@ -9,6 +8,7 @@ import {
   Tooltip,
   cn,
   usePagination,
+  Select,
 } from "@repo/ui";
 import { ReloadButton } from "components/reload-button";
 import { useAuctions } from "modules/auction/hooks/use-auctions";
@@ -22,6 +22,7 @@ import {
   Link as ScrollLink,
   Element as ScrollTargetElement,
 } from "react-scroll";
+import { sortAuction } from "modules/auction/utils/sort-auctions";
 
 const options = [
   { value: "created", label: "Created" },
@@ -32,9 +33,10 @@ const options = [
 ];
 
 export default function AuctionListPage() {
-  const [filters, setFilters] = useState<string[]>([]);
+  const [filters] = useState<string[]>([]);
   const [searchText, setSearchText] = useState<string>("");
   const [gridView, setGridView] = useState(false);
+  const [sortByStatus, setSortByStatus] = useState<string | undefined>();
   const { data: auctions, isLoading, refetch, isRefetching } = useAuctions();
 
   const filteredAuctions = filters.length
@@ -43,7 +45,18 @@ export default function AuctionListPage() {
         .filter((a) => !searchText.length || searchObject(a, searchText))
     : auctions;
 
-  const { rows, ...pagination } = usePagination(filteredAuctions, 9);
+  const sortedAuctions = filteredAuctions.sort((a, b) => {
+    if (a.status === sortByStatus && b.status === sortByStatus) {
+      //If they're both the same, order by starting date
+      return Number(a.start) - Number(b.start);
+    }
+    if (a.status === sortByStatus) return -1;
+    if (b.status === sortByStatus) return 1;
+
+    return sortAuction(a, b);
+  });
+
+  const { rows, ...pagination } = usePagination(sortedAuctions, 9);
 
   return (
     <div className="">
@@ -62,7 +75,13 @@ export default function AuctionListPage() {
           </Text>
           <div className="mx-auto mt-6 flex w-min gap-x-2">
             <ScrollLink to="auctions" offset={-10} smooth={true}>
-              <Button className="uppercase" size="lg">
+              <Button
+                onClick={() => {
+                  setSortByStatus("created");
+                }}
+                className="uppercase"
+                size="lg"
+              >
                 <div className="flex items-center">
                   Upcoming Sales
                   <div className="size-6">
@@ -86,18 +105,22 @@ export default function AuctionListPage() {
               <Text size="2xl">Token Launches</Text>
             </Tooltip>
             <div className="flex gap-x-2">
-              <DropdownChecker
-                setFilters={setFilters}
-                filters={filters}
-                options={options}
-                label="Auction Status"
-              />
               <IconnedInput
                 icon={<SearchIcon />}
                 className="placeholder:text-foreground"
                 placeholder="Search"
                 onChange={(e) => setSearchText(e.target.value)}
               />
+              <Select
+                triggerClassName="w-[120px]"
+                placeholder="Sort By"
+                options={options}
+                onChange={(value) => {
+                  console.log({ value });
+                  setSortByStatus(value);
+                }}
+              />
+
               <ToggleGroup
                 type="single"
                 defaultValue="list"
