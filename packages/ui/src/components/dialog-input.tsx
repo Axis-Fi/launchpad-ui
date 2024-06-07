2af55eaf6bc9bc4add445311f500369d9c6572b0
aphex <wenlogic@protonmail.com>
2024-06-07 14:01:29 +0100

    update date-picker, dialog-input and switch

--- packages/ui/src/components/dialog-input.tsx
+++ packages/ui/src/components/dialog-input.tsx
@@ -60,7 +60,7 @@ export function DialogInput<T>({ onChange, ...props }: DialogInputProps<T>) {
       triggerElement={
         <Button
           variant="input"
-          className="bg-surface-secondary flex w-full justify-start text-sm normal-case"
+          className="bg-surface-tertiary hover:bg-surface-secondary flex w-full justify-start text-sm normal-case"
         >
           {triggerContent}
         </Button>
