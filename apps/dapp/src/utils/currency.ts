3f86c9c46fe73f52a36b20485046f17917a62449
aphex <wenlogic@protonmail.com>
2024-06-25 13:40:24 +0100

    add token formatter

--- apps/dapp/src/utils/currency.ts
+++ apps/dapp/src/utils/currency.ts
@@ -1,10 +1,10 @@
 export const currencyFormatter = new Intl.NumberFormat("en-US", {
-  minimumFractionDigits: 2,
+  minimumFractionDigits: 0,
   maximumFractionDigits: 2,
 });
 
 /* Trims decimal based on the number of decimal places */
-export function trimCurrency(input: string | number): string {
+export function trimCurrency(input: string | number) {
   const value = Number(input);
 
   if (value === 0) return "0";
@@ -15,7 +15,7 @@ export function trimCurrency(input: string | number): string {
 
   if (value < 1 && value !== 0) {
     const absLog = -Math.floor(Math.log10(Math.abs(value)));
-    const decimalPlaces = Math.min(8, absLog + 2); // Allow at least 2 significant figures
+    const decimalPlaces = Math.min(8, absLog); // Allow at least 2 significant figures
     const adjustedValue = Number(value.toFixed(decimalPlaces));
     return adjustedValue.toString();
   } else {
