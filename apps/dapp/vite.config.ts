7032dc6bfc3703ed8c2c0981fb8023122f82fdbb
aphex <wenlogic@protonmail.com>
2024-07-18 16:21:44 +0100

    set page title based on environment

--- apps/dapp/vite.config.ts
+++ apps/dapp/vite.config.ts
@@ -1,13 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
+    transformHTML(mode),
     react(),
     tsconfigPaths(),
     nodePolyfills({ globals: { Buffer: true } }),
   ],
-});
+}));
+
+function transformHTML(mode: string) {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    name: "html-transform",
+    transformIndexHtml(html: string) {
+      if (env.VITE_ENVIRONMENT !== "production") {
+        return html.replace(
+          /<title>(.*?)<\/title>/,
+          `<title>Origin Testnet</title>`,
+        );
+      }
+    },
+  };
+}
