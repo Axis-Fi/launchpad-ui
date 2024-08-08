81c29dceb62f262c26b5205c4c713b25802bfe1e
aphex <wenlogic@protonmail.com>
2024-08-08 11:00:43 +0100

    fix auction start date card on AuctionCreated

--- apps/dapp/src/modules/auction/status/auction-created.tsx
+++ apps/dapp/src/modules/auction/status/auction-created.tsx
@@ -5,14 +5,14 @@ import { ProjectInfoCard } from "../project-info-card";
 
 export function AuctionCreated({ auction }: PropsWithAuction) {
   return (
-    <div className="auction-action-container h-full items-stretch gap-x-4 lg:block">
+    <div className="auction-action-container h-full items-stretch gap-x-4 lg:flex">
       <div className="lg:w-2/3 ">
         <AuctionLaunchMetrics auction={auction} />
         <ProjectInfoCard className="mt-4" auction={auction} />
       </div>
       <div className="items-strech h-full lg:w-1/3">
         <Card>
-          <Text className="text-center text-lg lg:text-sm">
+          <Text className="lg:text-md text-center text-lg">
             Auction starts in {auction.formatted?.startDistance} <br />
             at {auction.formatted?.startFormatted}.
           </Text>
