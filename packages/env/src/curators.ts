0cec39f855f06fbfb8ee20b49089cf3e19dc4842
aphex <wenlogic@protonmail.com>
2024-08-12 11:45:41 +0100

    add support for curator report

--- packages/env/src/curators.ts
+++ packages/env/src/curators.ts
@@ -9,5 +9,7 @@ export const allowedCurators: Curator[] = [
     avatar: "/images/revelo.jpg",
     description:
       "Revelo Intel is a firm that prepares institutional-grade research focusing on DeFi ecosystems, protocols, and narratives. Our comprehensive coverage allows users and investor to do proper due diligence in this fast-paced and complex space.",
+    reportURL:
+      "https://revelointel.com/wp-content/uploads/2024/08/Aurelius-Finance-Overview-by-Revelo-Intel.pdf",
   },
 ];
