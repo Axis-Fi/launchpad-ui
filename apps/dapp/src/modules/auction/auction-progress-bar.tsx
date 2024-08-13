c4a5e5d226910d646a5a28d069475ad7d5715a33
Ed <edmulraney@gmail.com>
2024-08-13 13:25:15 +0100

    chore: use new toggled usd component

--- apps/dapp/src/modules/auction/auction-progress-bar.tsx
+++ apps/dapp/src/modules/auction/auction-progress-bar.tsx
@@ -1,12 +1,11 @@
 import { PropsWithAuction } from "@repo/types";
-import { Progress, Text, useToggle } from "@repo/ui";
+import { Progress, Text } from "@repo/ui";
 import { calculateAuctionProgress } from "./utils/get-auction-progress";
-import { UsdAmount } from "./usd-amount";
+import { ToggledUsdAmount } from "./toggled-usd-amount";
 import { shorten } from "utils/number";
 
 /** Renders a progress bar with the amount of tokens commited in bids*/
 export default function AuctionProgressBar({ auction }: PropsWithAuction) {
-  const { isToggled } = useToggle();
   const progress = calculateAuctionProgress(auction);
   const bidAmount = auction.bids.reduce((total, b) => {
     total += Number(b.amountIn);
@@ -26,14 +25,11 @@ export default function AuctionProgressBar({ auction }: PropsWithAuction) {
           Bids
         </Text>
         <Text mono className="leading-0 -mt-1">
-          {isToggled ? (
-            <UsdAmount
-              token={auction.quoteToken}
-              amount={bidAmount}
-            />
-          ) : (
-            shorten(bidAmount)
-          )}
+          <ToggledUsdAmount
+            token={auction.quoteToken}
+            amount={bidAmount}
+            untoggledFormat={(bidAmount) => shorten(bidAmount)}
+          />
         </Text>
       </div>
     </Progress>
