fb1451d1ef91dad011604f7b1d33f5af4b4953a8
aphex <wenlogic@protonmail.com>
2024-08-20 11:10:13 +0100

    add permanent discord link

--- packages/env/src/metadata.ts
+++ packages/env/src/metadata.ts
@@ -1,4 +1,4 @@
-export const discord = "https://"; // TODO: add discord link
+export const discord = "https://discord.gg/tx9CrvPZWF";
 export const twitter = "https://twitter.com/axis_fi";
 export const website = "https://axis.finance";
 export const contact = "https://gkk12lnayco.typeform.com/to/ml7zCK52"; //"hello@axis.finance";
