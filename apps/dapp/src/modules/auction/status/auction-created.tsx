826685becc94c1721b5d0ed83bc9312554c2d3de
aphex <wenlogic@protonmail.com>
2024-08-27 11:36:45 +0100

    add token info card to auction created

--- apps/dapp/src/modules/auction/status/auction-created.tsx
+++ apps/dapp/src/modules/auction/status/auction-created.tsx
@@ -2,12 +2,14 @@ import { Card, Text } from "@repo/ui";
 import { PropsWithAuction } from "@repo/types";
 import { AuctionCoreMetrics } from "../auction-core-metrics";
 import { ProjectInfoCard } from "../project-info-card";
+import { TokenInfoCard } from "../token-info-card";
 
 export function AuctionCreated({ auction }: PropsWithAuction) {
   return (
     <div className="auction-action-container h-full items-stretch gap-x-4 lg:flex">
-      <div className="lg:w-2/3 ">
+      <div className="space-y-4 lg:w-2/3">
         <AuctionCoreMetrics auction={auction} />
+        <TokenInfoCard auction={auction} />
         <ProjectInfoCard className="mt-4" auction={auction} />
       </div>
       <div className="items-strech h-full lg:w-1/3">
