9a0a8a6a65d83ffa14a909e746dff42160de49d5
aphex <wenlogic@protonmail.com>
2024-09-11 12:12:06 +0100

    add connect step

---
+++ apps/dapp/src/modules/points/claim-steps/connect-step.tsx
@@ -0,0 +1,32 @@
+import { Text, Card } from "@repo/ui";
+import React from "react";
+import { useWizard } from "react-use-wizard";
+import { useAccount } from "wagmi";
+import ConnectButton from "components/connect-button";
+import { AxisPointsHeader } from "../claim-points-wizard";
+
+export function ConnectStep() {
+  const { isConnected } = useAccount();
+  const { nextStep } = useWizard();
+
+  React.useEffect(() => {
+    if (isConnected) nextStep();
+  }, [isConnected]);
+
+  return (
+    <Card className="bg-surface max-w-[304px]">
+      <AxisPointsHeader />
+      <div className="mt-6 space-y-2">
+        <Text>Welcome to the Axis Points Claim!</Text>
+        <Text>Create a user profile and claim your gift!</Text>
+        <Text>Link addresses and refer friends to multiply your points.</Text>
+        <ConnectButton />
+        <Text uppercase mono size="sm" className="text-foreground-secondary">
+          By using Axis, you agree to our{" "}
+          <span className="font-bold">Terms of Services</span> and our{" "}
+          <span className="font-bold">Privacy Policy </span>
+        </Text>
+      </div>
+    </Card>
+  );
+}
