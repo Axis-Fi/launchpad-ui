2d9066074d8d3ab6541fec97a12abd9c5e69a950
Ed <edmulraney@gmail.com>
2024-09-17 14:14:34 +0100

    feat: add green gradient border css class

--- apps/dapp/src/modules/app/user-profile.tsx
+++ apps/dapp/src/modules/app/user-profile.tsx
@@ -29,27 +29,25 @@ export function UserProfile() {
 
   return (
     <div className="flex items-center justify-center gap-x-4">
-      <div className={cn("auction-ended-gradient rounded-md p-[2px]")}>
-        <div className="bg-background px-sm flex items-center justify-between gap-x-2 rounded-md p-1">
-          <img className="size-8" src="/points-logo.png" />
-          {!isLoading && (
-            <Button
-              size="sm"
-              variant="ghost"
-              onClick={handleClick}
-              className={cn(
-                "text-foreground-highlight px-0 py-0 text-center leading-none",
-                !isRegistered && "text-nowrap",
-              )}
-            >
-              {isUserSignedIn
-                ? `${points != null ? trimCurrency(points) : "???"} points`
-                : isRegistered
-                  ? "Sign In"
-                  : "Claim Points"}
-            </Button>
-          )}
-        </div>
+      <div className="bg-background px-md green-gradient-border flex h-[48px] items-center justify-between gap-x-2 rounded-md p-1">
+        <img className="size-8" src="/points-logo.png" />
+        {!isLoading && (
+          <Button
+            size="sm"
+            variant="ghost"
+            onClick={handleClick}
+            className={cn(
+              "text-foreground-highlight px-0 py-0 text-center leading-none",
+              !isRegistered && "text-nowrap",
+            )}
+          >
+            {isUserSignedIn
+              ? `${points != null ? trimCurrency(points) : "???"} points`
+              : isRegistered
+                ? "Sign In"
+                : "Claim Points"}
+          </Button>
+        )}
       </div>
 
       <ConnectButton className="hidden w-auto md:block" size="md" />
