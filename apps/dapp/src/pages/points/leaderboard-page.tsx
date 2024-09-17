142164736f51ffc26be3a0596750eeccf384baa2
Ed <edmulraney@gmail.com>
2024-09-17 14:20:24 +0100

    feat: add banner for claiming points, and CTA for learning about eligibility

--- apps/dapp/src/pages/points/leaderboard-page.tsx
+++ apps/dapp/src/pages/points/leaderboard-page.tsx
@@ -1,16 +1,23 @@
-import { Card, Text } from "@repo/ui";
+import { Link } from "react-router-dom";
+import { Button, Card, Text } from "@repo/ui";
 import { PageContainer } from "modules/app/page-container";
+import { ClaimYourPointsBanner } from "modules/points/claim-your-points-banner";
 import { Leaderboard } from "modules/points/leaderboard";
 import { RecentJoins } from "modules/points/recent-joins";
 
 export function LeaderboardPage() {
   return (
     <>
-      <div className="axis-rainbow-reverse p-xl flex h-[224px] w-full flex-col items-center justify-end">
+      <div className="axis-rainbow-reverse p-xl gap-sm flex h-[296px] w-full flex-col items-center justify-end">
         <Text size="2xl">Axis points</Text>
+        <Button variant="secondary" asChild>
+          <Link to="#">Learn if you&apos;re eligible</Link>
+        </Button>
       </div>
 
       <PageContainer>
+        <ClaimYourPointsBanner />
+
         <div className="flex flex-row gap-x-4">
           <Card className="w-3/4">
             <Leaderboard />
