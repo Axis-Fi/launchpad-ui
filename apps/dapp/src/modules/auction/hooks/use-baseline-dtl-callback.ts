3879fb3f53d3086b36d5b69c13045aa34661100d
Jem <0x0xjem@gmail.com>
2024-10-08 16:57:06 +0400

    Add callback for fetching Baseline configuration

---
+++ apps/dapp/src/modules/auction/hooks/use-baseline-dtl-callback.ts
@@ -0,0 +1,101 @@
+import { axisContracts } from "@repo/deployments";
+import { Address, CallbacksType } from "@repo/types";
+import { getCallbacks } from "utils/contracts";
+import { useReadContract, useReadContracts } from "wagmi";
+
+export function useBaselineDTLCallback({
+  chainId,
+  lotId,
+  callback,
+}: {
+  chainId?: number;
+  lotId?: string;
+  baseTokenDecimals: number;
+  callback?: Address;
+}) {
+  // Determine if the callback is a baseline DTL callback
+  const callbackLower = (callback || "").toLowerCase();
+  const baselineAddresses = getCallbacks(
+    chainId || 0,
+    CallbacksType.BASELINE,
+  ).address.map((address) => address.toLowerCase());
+  const baselineAllowlistAddresses = getCallbacks(
+    chainId || 0,
+    CallbacksType.BASELINE_ALLOWLIST,
+  ).address.map((address) => address.toLowerCase());
+  const baselineCappedAllowlistAddresses = getCallbacks(
+    chainId || 0,
+    CallbacksType.BASELINE_CAPPED_ALLOWLIST,
+  ).address.map((address) => address.toLowerCase());
+  const baselineTokenAllowlistAddresses = getCallbacks(
+    chainId || 0,
+    CallbacksType.BASELINE_TOKEN_ALLOWLIST,
+  ).address.map((address) => address.toLowerCase());
+  const isBaselineDTLCallback =
+    baselineAddresses.includes(callbackLower) ||
+    baselineAllowlistAddresses.includes(callbackLower) ||
+    baselineCappedAllowlistAddresses.includes(callbackLower) ||
+    baselineTokenAllowlistAddresses.includes(callbackLower);
+
+  // Determine if callback's lot id matches the lot id of the auction
+  const { data: baselineLotId } = useReadContract({
+    abi: axisContracts.abis.baseline,
+    address: callback,
+    chainId,
+    functionName: "lotId",
+    args: [],
+  });
+  const lotIdMatches = baselineLotId === lotId;
+
+  const response = useReadContracts({
+    contracts: [
+      {
+        abi: axisContracts.abis.baseline,
+        address: callback,
+        chainId,
+        functionName: "auctionComplete",
+      },
+      {
+        abi: axisContracts.abis.baseline,
+        address: callback,
+        chainId,
+        functionName: "poolPercent",
+      },
+      {
+        abi: axisContracts.abis.baseline,
+        address: callback,
+        chainId,
+        functionName: "floorReservesPercent",
+      },
+      {
+        abi: axisContracts.abis.baseline,
+        address: callback,
+        chainId,
+        functionName: "recipient",
+      },
+      {
+        abi: axisContracts.abis.baseline,
+        address: callback,
+        chainId,
+        functionName: "poolTargetTick",
+      },
+    ],
+    query: {
+      enabled: isBaselineDTLCallback && lotIdMatches,
+    },
+  });
+
+  return {
+    ...response,
+    data:
+      response.isSuccess && response.data.length === 5
+        ? {
+            auctionComplete: response.data[0].result,
+            poolPercent: response.data[1].result,
+            floorReservesPercent: response.data[2].result,
+            recipient: response.data[3].result,
+            poolTargetTick: response.data[4].result,
+          }
+        : undefined,
+  };
+}
