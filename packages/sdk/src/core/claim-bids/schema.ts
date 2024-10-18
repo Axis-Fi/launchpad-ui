868e5fbfa3e12726c383b545f7cfaedbbef00655
aphex <wenlogic@protonmail.com>
2024-10-18 10:50:25 +0100

    make auctionType on sdk claim-bids required

--- packages/sdk/src/core/claim-bids/schema.ts
+++ packages/sdk/src/core/claim-bids/schema.ts
@@ -5,7 +5,7 @@ const schema = v.object({
   lotId: v.number(),
   bids: v.array(v.number()),
   chainId: v.number(),
-  auctionType: v.optional(v.enum_(AuctionType)),
+  auctionType: v.enum_(AuctionType),
 });
 
 export default schema;
