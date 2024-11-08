1ff92ed439a1c12eebdf73bb33e907a534b80c3e
Ed <edmulraney@gmail.com>
2024-11-08 12:41:07 +0000

    fix: use same typings

--- packages/sdk/src/react/hooks/use-launch.ts
+++ packages/sdk/src/react/hooks/use-launch.ts
@@ -1,6 +1,4 @@
-import type { UseQueryOptions } from "@tanstack/react-query";
 import {
-  BatchAuctionLot,
   type GetBatchAuctionLotQuery,
   useGetBatchAuctionLotQuery,
 } from "@repo/subgraph-client";
@@ -8,25 +6,32 @@ import { deployments } from "@repo/deployments";
 import { getLaunchId } from "../../core/utils";
 import { useSdk } from "./use-sdk";
 
+type UseGetBatchAuctionLotQueryOptions = Parameters<
+  typeof useGetBatchAuctionLotQuery
+>[2];
+
 type LaunchQueryConfig = {
   chainId: number;
   lotId: number;
-  options?: Partial<
-    UseQueryOptions<GetBatchAuctionLotQuery, unknown, BatchAuctionLot>
-  >;
+  options?: Partial<UseGetBatchAuctionLotQueryOptions>;
 };
+
 export const useLaunch = ({ chainId, lotId, options }: LaunchQueryConfig) => {
   const sdk = useSdk();
   const id = getLaunchId(chainId, lotId);
-  const isQueryEnabled = chainId != null && id != null && options?.enabled;
+  const isQueryEnabled =
+    chainId != null &&
+    id != null &&
+    (options?.enabled satisfies boolean | undefined);
 
   const subgraphUrl =
     sdk.config?.subgraph?.[chainId]?.url ?? deployments[chainId!].subgraphURL;
 
-  return useGetBatchAuctionLotQuery(
+  return useGetBatchAuctionLotQuery<GetBatchAuctionLotQuery["batchAuctionLot"]>(
     { endpoint: subgraphUrl },
     { id: id! },
     {
+      ...options,
       enabled: isQueryEnabled,
       select: (data) => data.batchAuctionLot,
     },
