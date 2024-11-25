33067998a430ffc4e5eb8af6eda7ddd5df93a86f
aphex <wenlogic@protonmail.com>
2024-11-25 13:45:28 +0000

    update dark mode

--- apps/dapp/src/pages/curator-launch-page.tsx
+++ apps/dapp/src/pages/curator-launch-page.tsx
@@ -43,6 +43,7 @@ const options = [
 type AuctionListPageProps = {
   curator?: Curator;
 };
+
 export default function CuratorLaunchPage({ curator }: AuctionListPageProps) {
   const { isTabletOrMobile } = useMediaQueries();
   const [userSettings, dispatch] = useAtom(auctionListSettingsAtom);
@@ -128,7 +129,11 @@ export default function CuratorLaunchPage({ curator }: AuctionListPageProps) {
             <div className="mb-10 flex w-full flex-col items-center justify-end">
               <div className="relative mb-2 flex justify-center self-center text-center">
                 <div className="border-primary absolute inset-0 top-3 -z-10 -ml-10 size-full w-[120%] border bg-neutral-950 blur-2xl" />
-                <Text size="7xl" mono className="mx-auto text-neutral-200">
+                <Text
+                  size="7xl"
+                  mono
+                  className="dark:text-foreground mx-auto text-neutral-200"
+                >
                   {curator?.name}
                 </Text>
               </div>
