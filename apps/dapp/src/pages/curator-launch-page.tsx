ad01d19ee4cd6dce2a5c14ff848a44785f1d5ea1
aphex <wenlogic@protonmail.com>
2024-12-03 15:21:10 +0000

    * add baseline allocated allowlist address * add curator type options and revelo metadata

--- apps/dapp/src/pages/curator-launch-page.tsx
+++ apps/dapp/src/pages/curator-launch-page.tsx
@@ -126,13 +126,15 @@ export default function CuratorLaunchPage({ curator }: AuctionListPageProps) {
             <div className="mb-10 flex w-full flex-col items-center justify-end">
               <div className="relative mb-2 flex justify-center self-center text-center">
                 <div className="border-primary absolute inset-0 top-3 -z-10 -ml-10 size-full w-[120%] border bg-neutral-950 blur-2xl" />
-                <Text
-                  size="7xl"
-                  mono
-                  className="dark:text-foreground mx-auto text-neutral-200"
-                >
-                  {curator?.name}
-                </Text>
+                {!curator?.options?.hideName && (
+                  <Text
+                    size="7xl"
+                    mono
+                    className="dark:text-foreground mx-auto text-neutral-200"
+                  >
+                    {curator?.name}
+                  </Text>
+                )}
               </div>
             </div>
           </div>
