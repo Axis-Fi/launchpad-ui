91111d240ba218ab8d4ca7f6ac4ddc034e58a30b
Ed <5167260+edmulraney@users.noreply.github.com>
2024-12-11 23:23:04 +0000

    chore: replace internal types package with public version

--- packages/types/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./auction";
-export * from "./auction-modules";
-export * from "./callbacks";
-export * from "./chain";
-export * from "./curator";
-
-export type * from "./axis-contracts";
-export type * from "./token";
-export type * from "./subgraph-queries";
