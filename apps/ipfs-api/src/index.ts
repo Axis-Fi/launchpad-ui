8e744fa71cba92e6518506b383dbd1e830d4fd40
Ed <5167260+edmulraney@users.noreply.github.com>
2025-01-06 02:42:42 +0000

    fix: only use http ipfs api locally

--- apps/ipfs-api/src/index.ts
+++ apps/ipfs-api/src/index.ts
@@ -31,7 +31,7 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 day
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV !== "development",
       sameSite: "lax",
     },
   }),
