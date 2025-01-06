f19c9c6132a58b556dc09e85014ea6b2b2f85839
Ed <5167260+edmulraney@users.noreply.github.com>
2025-01-06 10:08:39 +0000

    fix: fix ipfs api cors

--- apps/ipfs-api/src/index.ts
+++ apps/ipfs-api/src/index.ts
@@ -32,7 +32,7 @@ app.use(
       maxAge: 1000 * 60 * 60 * 24, // 1 day
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
-      sameSite: "lax",
+      sameSite: "none",
     },
   }),
 );
