09d6c3b774ac38c7035b1aa122b6a1de2e2ece5a
aphex <wenlogic@protonmail.com>
2025-01-09 23:26:00 +0000

    fix percentage slider returning strings instead of array

--- packages/ui/src/components/percentage-slider.tsx
+++ packages/ui/src/components/percentage-slider.tsx
@@ -1,7 +1,6 @@
 import type { ControllerRenderProps, FieldValues, Path } from "react-hook-form";
 import { cn } from "..";
 import { Input, Slider } from "./primitives";
-import { useEffect } from "react";
 
 type PercentageSliderProps<
   T extends FieldValues = FieldValues,
@@ -27,14 +26,6 @@ export function PercentageSlider<
   max = 100,
   ...props
 }: PercentageSliderProps<T, K>) {
-  useEffect(() => {
-    // If you look at the console you'll see the creat-auction page
-    // sends through values like [10], and then a string "10",
-    // which breaks it as we take the first char of the string field.value[0]
-    // and set the value to that, which is below the min.
-    // Need to figure out why the form is doing that.
-    console.log("xxx", field, field.value, field.value?.[0]);
-  }, [field.value]);
   return (
     <div className={cn("flex items-center", props.className)}>
       <Input
@@ -43,14 +34,12 @@ export function PercentageSlider<
         value={`${field.value?.[0] ?? defaultValue}%`}
       />
       <Slider
-        {...field}
         className={cn("cursor-pointer", props.sliderClassName)}
         min={min}
         max={max}
         defaultValue={[defaultValue ?? 0]}
         value={Array.isArray(field.value) ? field.value : [field.value]}
         onValueChange={(v) => {
-          console.log("CHANGED", v);
           field.onChange(v);
         }}
       />
