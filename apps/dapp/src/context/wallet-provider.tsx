7b8649980f53e15a383bbd75ddb3f05a0784d6ed
Ed <5167260+edmulraney@users.noreply.github.com>
2025-03-06 17:37:52 +0000

    feat: add auto signer provider for testing

--- apps/dapp/src/context/wallet-provider.tsx
+++ apps/dapp/src/context/wallet-provider.tsx
@@ -1,34 +1,7 @@
 import "@rainbow-me/rainbowkit/styles.css";
 
 import { PropsWithChildren } from "react";
-import {
-  RainbowKitProvider,
-  connectorsForWallets,
-  midnightTheme,
-} from "@rainbow-me/rainbowkit";
-import {
-  injectedWallet,
-  rainbowWallet,
-  frameWallet,
-  walletConnectWallet,
-} from "@rainbow-me/rainbowkit/wallets";
-
-const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID;
-
-export const connectors = connectorsForWallets(
-  [
-    {
-      groupName: "Common",
-      wallets: [
-        injectedWallet,
-        rainbowWallet,
-        frameWallet,
-        walletConnectWallet,
-      ],
-    },
-  ],
-  { projectId, appName: "Axis Finance" },
-);
+import { RainbowKitProvider, midnightTheme } from "@rainbow-me/rainbowkit";
 
 export default function WalletProvider(props: PropsWithChildren) {
   return (
